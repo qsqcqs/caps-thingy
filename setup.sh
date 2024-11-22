8dc703c066e5036bb4fbfc54dc7803e84accfa69
qsqcqs <118565709+qsqcqs@users.noreply.github.com>
2024-11-22 00:16:53 +0000

    uses proper path and actually inits

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@
 #./bin/python (the venv included)
 #./caps.py (the program)
 # > /dev/null (so there isnt any output
-echo -e "\n./bin/python ./caps.py > /dev/null  &\n" >> ~/.xprofile
+echo -en `realpath .` >> ~/.xprofile
+echo -en "/bin/python " >> ~/.xprofile
+echo -en `realpath .` >> ~/.xprofile
+echo -e "/caps.py > /dev/null  &\n" >> ~/.xprofile
+#added realpath so it actually like...uses the path to the file lol
+#actully runs it
+./bin/python ./caps.py > /dev/null  &
